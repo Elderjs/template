7d984bfaad643bddf3da72f894c68d0eb1dfe011
Nick Reese <nick@nicholasreese.com>
2020-08-11 13:12:56 -0400

    fix: 🐛 small change so netlify picks it up

--- src/afterBuild.js
+++ src/afterBuild.js
@@ -3,5 +3,4 @@ const { getConfig } = require('@elderjs/elderjs');
 const path = require('path');
 
 const config = getConfig();
-
 console.log(glob.sync(path.join(process.cwd(), config.locations.public) + '/**/**'));
