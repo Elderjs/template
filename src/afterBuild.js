b3833a6e29d0d7eb67600e4d21977c4136b329f6
Nick Reese <nick@nicholasreese.com>
2020-08-11 13:51:39 -0400

    check what netlify is seeing after build

---
+++ src/afterBuild.js
@@ -0,0 +1,7 @@
+const glob = require('glob');
+const { getConfig } = require('@elderjs/elderjs');
+const path = require('path');
+
+const config = getConfig();
+console.log(glob.sync(path.join(process.cwd(), config.locations.public) + '/**/**'));
+// console.log(glob.sync(path.join(process.cwd()) + '/**/**'));
