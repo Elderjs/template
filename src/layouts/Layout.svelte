34b3f98dfb05cb9dd035c98791a5af8286046a9b
Devin Bayer <dev@doubly.so>
2020-09-10 22:37:08 +0200

    Make assets location portable (#12)

--- src/layouts/Layout.svelte
+++ src/layouts/Layout.svelte
@@ -1,5 +1,6 @@
 <script>
   export let routeHTML, settings;
+  var assets_dir = settings.locations.assets.replace(settings.locations.public, '/');
 </script>
 
 <style>
@@ -19,7 +20,7 @@
 </style>
 
 <svelte:head>
-  <link rel="stylesheet" href="{settings.locations.assets.replace('./public', '')}style.css" />
+  <link rel="stylesheet" href="{assets_dir}style.css" />
   <link rel="stylesheet" href="https://unpkg.com/balloon-css/balloon.min.css" />
 </svelte:head>
 <div class="container">
