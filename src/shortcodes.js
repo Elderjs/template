e592875f9a0ed28184213753d1c19be0eb0ceb61
Nick Reese <nick@Nicks-MacBook-Pro.local>
2020-09-15 16:16:58 -0400

    feat: 🎸 small shortcode change

--- src/shortcodes.js
+++ src/shortcodes.js
@@ -2,8 +2,8 @@
  * Shortcodes are a useful way of making content that lives in a CMS or in markdown files dynamic.
  *
  * By default, Elder.js ships with a {{svelteComponent name="" props="" options="" /}} shortcode.
- * Try adding a clock to one of your markdown files with `{{svelteComponent name="Clock" options='{"preload":true}' props='{"foo": true}' /}}`
- *
+ * Try adding a clock to one of your markdown files with:
+ * `{{svelteComponent name="Clock" options='{"preload":true}' props='{"foo": true}' /}}`
  *
  */
 
