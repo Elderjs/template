b36c12c819a0ed7b632c8780bfbf50ad3c10dc6e
Steve Anderson <steve@twindx.com>
2020-10-03 13:45:38 +0100

    Fix some spelling and grammar in Home.svelte
    A handful of edits for spelling and grammar. 😃

--- src/routes/home/Home.svelte
+++ src/routes/home/Home.svelte
@@ -87,12 +87,12 @@
   <p>Woot! You've got Elder.js installed. This template is designed to show you the ropes of how things work.</p>
 
   <p>
-    We've tried to make this site a bit of a tutorial but be sure to check out the full <a href="https://elderguide.com/tech/elderjs/">Elder.js
+    We've tried to make this site a bit of a tutorial, but be sure to check out the full <a href="https://elderguide.com/tech/elderjs/">Elder.js
       docs.</a>
   </p>
 
   <p>
-    Enjoy playing around with Elder.js and if you hit a snag with the template open a <a href="https://github.com/Elderjs/template/issues">GitHub
+    Enjoy playing around with Elder.js and, if you hit a snag with the template, open a <a href="https://github.com/Elderjs/template/issues">GitHub
       issue.</a>
   </p>
 </div>
@@ -108,7 +108,7 @@
 <div class="about">
   <h2>About This Template</h2>
   <p>
-    This example project is made up of 4 routes, you can find them by looking within the <span
+    This example project is made up of 4 routes; you can find them by looking within the <span
       class="code">./src/routes/</span> folder. They are:
   </p>
 
@@ -120,18 +120,18 @@
         href={helpers.permalinks.blog({ slug: 'getting-started' })}>'Getting Started'</a> .
     </li>
     <li>
-      Hooks - These are how you customize Elder.js. Details are below and we've used the <span
+      Hooks - These are how you customize Elder.js. Details are below, and we've used the <span
         class="code">hookInterface</span> to build out dedicated pages for each hook as well.
     </li>
   </ul>
 
   <p>
     The goal in showing off these 3 routes is to give you enough of an example to see how a site is built with Elder.js
-    but one that isn't too complex to overwhelm you.
+    (but one that isn't too complex to overwhelm you).
   </p>
-  <h3>Development Enviroment:</h3>
+  <h3>Development Environment:</h3>
   <p>
-    If you ran <span class="code">npm start</span> to see this page, we recommend you stop that command instead open two
+    If you ran <span class="code">npm start</span> to see this page, we recommend you stop that command. Instead, open two
     terminals and run:
   </p>
   <ol>
@@ -144,8 +144,8 @@
   </ol>
 
   <p>
-    <strong>Note:</strong> Make sure you also check out <code>npm run build</code> which will statically generate this same
-    site so it can be deployed with a static site host such as Netlify, Cloudflare Workers, Vercel, or S3.
+    <strong>Note:</strong> Make sure you also check out <code>npm run build</code>, which will statically generate this same
+    site so that it can be deployed with a static site host such as Netlify, Cloudflare Workers, Vercel, or S3.
   </p>
 </div>
 
@@ -154,16 +154,16 @@
     <h2>Partial Hydration:</h2>
     <p>Svelte.js shines at bringing interactivity to otherwise static websites.</p>
     <p>
-      By default, Elder.js statically renders Svelte components only mounting them in the browser when it encounters a
+      By default, Elder.js statically renders Svelte components, only mounting them in the browser when it encounters a
       Svelte component which includes the
       <!-- Note: the actual prop is 'hydrate-client={}' but Svelte doesn't render empty objects-->
       <code>hydrate-client={JSON.stringify({})}</code> prop.
     </p>
     <p>
-      The <a href="https://svelte.dev/examples#clock">clock</a> on the this page is an example of a component that has been
+      The <a href="https://svelte.dev/examples#clock">clock</a> on this page is an example of a component that has been
       hydrated on the client.
     </p>
-    <p>This approach makes it easy to build SEO friendly websites with Svelte for interactivity when needed.</p>
+    <p>This approach makes it easy to build SEO friendly websites, with Svelte for interactivity when needed.</p>
     <p>By default all hydrated components are lazy loaded with an intersection observer.</p>
   </div>
   <div class="right">
