952ff77901470fb84c861f0ebe184feecb25c3c1
Nick Reese <nick@mbp.com>
2020-10-22 17:18:26 -0400

    refactor: 💡 clean up server

--- src/server.js
+++ src/server.js
@@ -1,34 +1,22 @@
-/* eslint-disable no-unused-vars */
 require('dotenv').config();
 const polka = require('polka');
 const cors = require('cors');
 const compression = require('compression');
 const bodyParser = require('body-parser');
 const sirv = require('sirv');
+const dev = process.env.NODE_ENV === 'development';
 
-const { Elder, getConfig } = require('@elderjs/elderjs');
+const { Elder } = require('@elderjs/elderjs');
 const elder = new Elder({ context: 'server' });
-const { distDir } = getConfig();
-
-const SERVER_PORT = process.env.SERVER_PORT || 3000;
 
 const server = polka();
-
 server.use(cors());
-
-server.use((req, res, next) => {
-  res.setHeader('Cache-Control', 'no-store');
-  next();
-});
-
 server.use(compression({ level: 6 }));
-
 server.use(bodyParser.urlencoded({ extended: false }), bodyParser.json());
-
 server.use(elder.server);
+server.use(sirv(elder.settings.distDir, { dev }));
 
-server.use(sirv(distDir, { dev: true }));
-
+const SERVER_PORT = process.env.SERVER_PORT || 3000;
 server.listen(SERVER_PORT, (err) => {
   if (err) {
     console.log(err);
