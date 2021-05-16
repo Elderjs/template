04c7294a7ec6ca214817d34db99802d189bc44e1
Chris Grieger <fourcube@users.noreply.github.com>
2021-05-16 20:37:35 +0200

    Add missing subdirectory in simple route steps (#46)

--- src/routes/simple/route.js
+++ src/routes/simple/route.js
@@ -11,7 +11,7 @@ module.exports = {
     return {
       title: 'Elder.js Route: An Overview',
       steps: [
-        `Step 1: All routes require a route.js and a svelte template. Look at ./src/simple/route.js to follow along.`,
+        `Step 1: All routes require a route.js and a svelte template. Look at ./src/routes/simple/route.js to follow along.`,
         `Step 2: We define an 'all()' function that returns an array of 'request' objects.`,
         `Step 3: We define a permalink function that transforms the 'request' objects from 'all()' into permalinks.`,
         `Step 4: We define a data function that makes data available in your svelte template.`,
