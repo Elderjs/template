f2c618ab258e1566ae41428de2c1a70fc29888e1
Nick Reese <code@nicholasreese.com>
2021-08-09 19:32:06 +0200

    update note on origin

--- elder.config.js
+++ elder.config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 module.exports = {
-  origin: 'https://example.com', // TODO: update this. Add your domain name without trailing slash.
+  origin: 'https://example.com', // TODO: update this. The URL of your site's root, without a trailing slash
   lang: 'en',
   srcDir: 'src',
   distDir: 'public',
