587477d45304e79f0630e61723c579c724679a32
Mark Jaquith <mark@jaquith.me>
2022-07-12 21:57:16 -0400

    Trim trailing whitespace

--- src/hooks.js
+++ src/hooks.js
@@ -4,19 +4,19 @@ const fs = require('fs-extra');
 const os = require('os');
 
 /**
- * Hooks! 
- * 
+ * Hooks!
+ *
  * Lifecycle hooks are the backbone of how you can have complete control over the output of your site.
- * Hooks are enforced via the hookInterface 'contract' defined here: 
+ * Hooks are enforced via the hookInterface 'contract' defined here:
         https://github.com/Elderjs/elderjs/blob/master/src/hookInterface/hookInterface.ts
  *
  * If you read the hookInterface spec closely you'll see that each defined hook gets specific 'props' along with which of those props is 'mutable'.
- * 
- * If you're a fan of 'pure' functions in JS, mutating props will probably set off alarm bells in your head. Fear not, instead of burying 
+ *
+ * If you're a fan of 'pure' functions in JS, mutating props will probably set off alarm bells in your head. Fear not, instead of burying
  * what is mutating things deep in your application, you'll know it is probably in this file.
  *
- * Also, to help keep mutation predictable each 'hook' limits which 'props' can be manipulated and where. 
- * 
+ * Also, to help keep mutation predictable each 'hook' limits which 'props' can be manipulated and where.
+ *
  */
 
 const hooks = [
